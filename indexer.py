75098904096cf501fee49b93b5cdb2884fd6b420
JCMiguel <20179139+JCMiguel@users.noreply.github.com>
2023-04-10 21:27:04 -0300

    Añade validaciones de campos al cargar fichas y boceto de template para el config

--- indexer.py
+++ indexer.py
@@ -8,8 +8,11 @@ import logging.config
 import traceback
 import numpy as np
 import pandas as pd
+from itertools import chain, repeat
+from datetime import datetime
 import os
 import sys
+
 
 # Variables globales de comportamiento
 __debug_flag = False
@@ -17,19 +20,39 @@ __debug_flag = False
 #  1. Sirve como plantilla de ficha en todos los métodos del CRUD
 #  2. Sirve para especificar, cuando se levanta el .csv, qué tipo de dato tiene cada columna
 # Cualquier cambio que haya que hacer en las fichas, debe hacerse también en este diccionario
-__base_dict_df = {'file': 'string',
-                  'title':'string',
-                  'author':'string',
-                  'repo':'string',
-                  'year':'int32',
-                  'date':'datetime64[ns]',
-                  'abstract':'string',
-                  'keywords':'string',
-                  'refs':'string',
+# TODO: Migrar estructura del diccionario al config.yml
+# TODO: Iterar sobre la estructura del diccionario e invocar input_a_text o input_a_number,
+#       en función del tipo de dato que esté definido en el diccionario
+__base_dict_df = {'file': 'string',             # Nombre del archivo - artículo descargado
+                  'title':'string',             # Título del artículo
+                  'author':'string',            # Autor(es) del artículo
+                  'repo':'string',              # Repositorio donde fue hallado el artículo
+                  'year':'int32',               # Año de publicación
+                  'date':'datetime64[ns]',      # Fecha de fichaje
+                  'abstract':'string',          # Abstract
+                  'keywords':'string',          # Palabras clave del artículo 
+                  'refs':'string',                  
                   'pros':'string',
                   'cons':'string',
                   'opps':'string'}
 
+def input_a_date(message:str) -> str:
+    bad_input_msg = "Se aceptan solo caracteres imprimibles!\n" + message
+    prompts = chain([message], repeat('\n'.join([bad_input_msg, message])))
+    replies = map(input, prompts)
+    return next(filter(str.isprintable, replies)) #valid_response
+
+def input_a_text(message:str) -> str:
+    bad_input_msg = "Se aceptan solo caracteres imprimibles!\n" + message
+    prompts = chain([message], repeat('\n'.join([bad_input_msg, message])))
+    replies = map(input, prompts)
+    return next(filter(str.isprintable, replies)) #valid_response
+
+def input_a_number(message:str) -> int:
+    bad_input_msg = "El dato debe ser numérico!\n" + message
+    prompts = chain([message], repeat('\n'.join([bad_input_msg, message])))
+    replies = map(input, prompts)
+    return next(filter(str.isdigit, replies)) #valid_response
 
 def load_dataframe(filename) -> pd.DataFrame:
     fichas = pd.read_csv(filename, index_col=0)
@@ -59,7 +82,7 @@ def get_index_card(args) -> int:
                 print(' > Autor(es):\n    '+ ficha['author'].values[i])
                 print(' > Medio de publicación:\n    '+ ficha['repo'].values[i])
                 print(' > Año de publicación:\n    '+ str(ficha['year'].values[i]))
-                print(' > Fecha de búsqueda:\n    '+ str(ficha['date'].values[i]))
+                print(' > Fecha de fichaje:\n    '+ str(ficha['date'].values[i]))
                 print(' > Abstract:\n    '+ ficha['abstract'].values[i])
                 print(' > Palabras clave:\n    '+ ficha['keywords'].values[i])
                 print(' > Referencias importantes:\n    '+ ficha['refs'].values[i])
@@ -95,34 +118,28 @@ def save_index_card(args) -> int:
     print('········· Generación de nueva ficha ·········')
     print('Ingrese los datos pedidos a continuación')
     new_dict = dict.fromkeys(__base_dict_df.keys())
-    #TODO: Validar datos ingresados por el usuario
-    new_dict['file']        = input(' > Nombre de archivo:\n    ')
-    new_dict['title']       = input(' > Título:\n    ')
-    new_dict['author']      = input(' > Autor(es):\n    ')
-    new_dict['repo']        = input(' > Medio de publicación:\n    ')
-    new_dict['year']        = input(' > Año de publicación:\n    ')
-    new_dict['date']        = input(' > Fecha de búsqueda:\n    ')
-    new_dict['abstract']    = input(' > Abstract:\n    ')
-    new_dict['keywords']    = input(' > Palabras clave:\n    ')
-    new_dict['refs']        = input(' > Referencias importantes:\n    ')
-    new_dict['pros']        = input(' > Fortalezas:\n    ')
-    new_dict['cons']        = input(' > Debilidades:\n    ')
-    new_dict['opps']        = input(' > Oportunidades:\n    ')
+    new_dict['file']        = input_a_text(' > Nombre de archivo:\n    ')
+    new_dict['title']       = input_a_text(' > Título:\n    ')
+    new_dict['author']      = input_a_text(' > Autor(es):\n    ')
+    new_dict['repo']        = input_a_text(' > Medio de publicación:\n    ')
+    new_dict['year']        = input_a_number(' > Año de publicación:\n    ')
+    new_dict['date']        = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    new_dict['abstract']    = input_a_text(' > Abstract:\n    ')
+    new_dict['keywords']    = input_a_text(' > Palabras clave:\n    ').lower()
+    new_dict['refs']        = input_a_text(' > Referencias importantes a otros archivos:\n    ')
+    new_dict['pros']        = input_a_text(' > Fortalezas:\n    ')
+    new_dict['cons']        = input_a_text(' > Debilidades:\n    ')
+    new_dict['opps']        = input_a_text(' > Oportunidades:\n    ')
 
     if(os.path.isfile('fichas.csv')):
         # Abro el csv con pandas
         fichas = load_dataframe('fichas.csv')
         new_ficha = pd.DataFrame(new_dict, index=[fichas.index[-1]+1])
         frames = [fichas, new_ficha]
-
-        #print('DEBUG')
-        #print(fichas)
-        #print('----------------------------------------------------')
-        #print(new_ficha)
-
         fichas = pd.concat(frames)
     else:
         fichas = pd.DataFrame(new_dict, index=[0])
+        fichas.astype(__base_dict_df)
     
     fichas.to_csv('fichas.csv', encoding='utf-8')
     return 0
@@ -169,18 +186,6 @@ def main() -> int:
         __debug_flag = False
 
     __success_flag = False
-    #if args.mode == 'get':
-    #    print('TODO: Get Index Card')
-    #elif args.mode == 'save':
-    #    print('TODO: Save Index Card')
-    #elif args.mode == 'edit':
-    #    print('TODO: Edit Index Card')
-    #elif args.mode == 'delete':
-    #    print('TODO: Delete Index Card')
-    #elif args.mode == 'list':
-    #    print('TODO: List all index cards')
-    #else:
-    #    print('ERROR: modo no permitido')
 
     print("Fin de ejecución")
     exit(0)
