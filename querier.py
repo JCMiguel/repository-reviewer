8e2fdc16da1c9b960a8c37978d682ebf18230284
JCMiguel <20179139+JCMiguel@users.noreply.github.com>
2023-08-11 20:53:07 -0300

    Borra código sobrante en querier.py

--- querier.py
+++ querier.py
@@ -2,6 +2,7 @@
 # -*- coding: utf8
 
 import argparse
+from engine.querier import querier
 import yaml
 import logging
 import logging.config
@@ -19,42 +20,7 @@ parser.add_argument('--query', dest='query', type=str, required=False)
 parser.add_argument('--content', dest='content', type=str, required=False)
 
 
-def read_yaml(file_path):
-    with open(file_path, "r") as f:
-        return yaml.safe_load(f)
-
-
-def querier(debug: bool, query: str = "", content: str = "", from_year: str = "", title: str = "", arguments = None):
-    print("Cargando archivo de configuración")
-    cfg = read_yaml("config/querier_config.yml") # TODO: Pendiente hacer chequeo de errores
-
-    print(debug)
-    print("Cargando clases de repositorios")
-    for repo in cfg['repos'].keys():
-        try:
-            # La línea siguiente invoca a la clase dentro del package.
-            # Ejemplo: invoca al constructor ieee() de repos.ieee_def
-            if cfg['repos'][repo]['enabled'] is True:
-                id = getattr(globals()[repo + '_def'], repo)(cfg['repos'][repo], cfg['params'], __debug_flag)
-                if id is not None:
-                    id.say_hello()
-                    if args.query == "" or args.query is None:
-                        id.add_query_param(args.content, 'content')
-                        id.add_query_param(args.fromYear, 'from_year')
-                        id.add_query_param(args.title, 'title')
-                        id.add_query_param(args.toYear, 'to_year')
-                        id.add_query_param(args.abstract, 'abstract')
-                    else:
-                        # TODO: Si me funciona con IEEE, tengo que ver cómo hacerlo para scopus.
-                        id.load_query(args.query)
-                    id.search()
-                    del id
-        except Exception:
-            traceback.print_exc()
-    print("Fin de ejecución")
-
-
-if __name__ == "__main__" :
+if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.debug:
